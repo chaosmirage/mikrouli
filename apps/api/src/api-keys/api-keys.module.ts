75fb4cb0c6dae56517b92c3342e2f0da02bdb39e
Adel Khamatov <hamatovadel@gmail.com>
2026-04-26 03:37:32 +0300

    fix(keys): also export ApiKeyAuthGuard for downstream injection

--- apps/api/src/api-keys/api-keys.module.ts
+++ apps/api/src/api-keys/api-keys.module.ts
@@ -12,6 +12,6 @@ import { BearerOrApiKeyGuard } from './bearer-or-api-key.guard';
   imports: [TypeOrmModule.forFeature([ApiKey]), UsersModule, AuthModule],
   providers: [ApiKeysService, ApiKeyAuthGuard, BearerOrApiKeyGuard],
   controllers: [ApiKeysController],
-  exports: [BearerOrApiKeyGuard, AuthModule],
+  exports: [BearerOrApiKeyGuard, ApiKeyAuthGuard, AuthModule],
 })
 export class ApiKeysModule {}
